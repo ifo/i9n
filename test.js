c4decd9eced3b7fc9c38ccd27966549d4cdb19d5
Steve McCarthy <steve@redlua.com>
2017-09-02 16:51:08 -0400

    Add tests for the ruin function

--- test.js
+++ test.js
@@ -26,3 +26,15 @@ test('i9n bad input tests', (t) => {
   t.equal(i9n.i9n(null), null);
   t.end();
 });
+
+test('ruin various sentences', (t) => {
+  t.equal(i9n.ruin('two words'), 't1o w3s');
+  t.equal(i9n.ruin('Short sentence.'), 'S3t s6e.');
+  t.equal(i9n.ruin('end of sentence has "quotes."'), 'e1d o0f s6e h1s "q4s."');
+  t.equal(i9n.ruin('tabs	are	fine'), 't2s\ta1e\tf2e');
+  t.equal(i9n.ruin(`handle
+newlines fine too`), 'h4e\nn6s f2e t1o');
+	t.equal(i9n.ruin(`combine	tabs spaces
+and newlines	without issue`), 'c5e\tt2s s4s\na1d n6s\tw5t i3e');
+  t.end();
+});
